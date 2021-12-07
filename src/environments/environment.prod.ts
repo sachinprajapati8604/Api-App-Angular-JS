145c0974d89bec1b6ad187840478d91455d56f2d
Sachin Prajapati <53299703+sachinprajapati8604@users.noreply.github.com>
2021-12-07 14:46:49 +0530

    product api url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
   api_url:'https://jsonplaceholder.typicode.com/',
-  api_prod_url:'https://demo7242716.mockable.io/',
+  // api_prod_url:'https://demo7242716.mockable.io/',
+  api_prod_url:'https://fakestoreapi.com/',
+  api_unsplash_url:"https://api.unsplash.com/photos/?client_id=Eo9TX6CUMOmxFFIdUHfyoDv3SYnpPqw78DKHPsOnWYU&per_page=100&topics=technology,science,nature",
 };
